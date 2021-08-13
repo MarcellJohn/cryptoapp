dfedb2fbecd2e6f497f7c9bd0a9ea245b344a627
John Petersen <marcellpetersen@outlook.com>
2021-08-13 19:14:24 +0200

    JSON og api nørkleri

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,15 @@
 class HomeController < ApplicationController
-  def index
-  end
+def index
+  require "net/http"
+  require "json"
 
-  def about
-  end
+  @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=10&cmc_pro_api_key=9cf6fc41-c5ee-438f-ba4e-3ca8321bb932"
+  @uri = URI(@url)
+  @response = Net::HTTP.get(@uri)
+  @coins = JSON.parse(@response)
+end 
 
+def about
 end
 
-
-
-
+end
